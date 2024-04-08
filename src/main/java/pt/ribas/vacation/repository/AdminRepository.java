bb12c393e70440defb8648aa88b1dda778364d81
lukalomidze <lomidzeluka.w@gmail.com>
2024-04-08 12:39:02 +0100

    fixed incorrect import in AdminRepository

--- src/main/java/pt/ribas/vacation/repository/AdminRepository.java
+++ src/main/java/pt/ribas/vacation/repository/AdminRepository.java
@@ -1,7 +1,8 @@
 package pt.ribas.vacation.repository;
 
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import pt.ribas.vacation.entity.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long>{
 
