6ed650c7bc2db2eb3e48be99fbd969f4587a7304
lukalomidze <lomidzeluka.w@gmail.com>
2024-04-15 18:14:29 +0100

    Added entity retrieval by username field

--- src/main/java/pt/ribas/vacation/repository/AdminRepository.java
+++ src/main/java/pt/ribas/vacation/repository/AdminRepository.java
@@ -1,9 +1,11 @@
 package pt.ribas.vacation.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pt.ribas.vacation.entity.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long>{
-
+    Optional<Admin> findByUsername(String username);
 }
