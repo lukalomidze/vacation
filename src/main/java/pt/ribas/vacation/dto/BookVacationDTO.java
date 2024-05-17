3ce269c60b9231754131f2b8b9c3742da06b3c4d
lukalomidze <lomidzeluka.w@gmail.com>
2024-05-17 19:33:58 +0100

    added future or present validation to vacation booking dates

--- src/main/java/pt/ribas/vacation/dto/BookVacationDTO.java
+++ src/main/java/pt/ribas/vacation/dto/BookVacationDTO.java
@@ -2,6 +2,7 @@ package pt.ribas.vacation.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,9 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookVacationDTO {
+    @FutureOrPresent
     @NotNull(message = "startDate must be provided in request body")
     private LocalDate startDate;
 
+    @FutureOrPresent
     @NotNull(message = "endDate must be provided in request body")
     private LocalDate endDate;
 }
