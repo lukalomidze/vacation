e66f3ee6bd3da143296594ce8c90b97f15fd14d6
lukalomidze <llomidze5@gmail.com>
2024-11-25 11:19:52 +0400

    added error handling logic

---
+++ src/main/java/pt/ribas/vacation/controller/ErrorController.java
@@ -0,0 +1,26 @@
+package pt.ribas.vacation.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @RequestMapping("/error")
+    public String error(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "redirect:/";
+            }
+        }
+
+        return null;
+    }
+}
